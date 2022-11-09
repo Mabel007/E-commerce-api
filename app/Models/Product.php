3b06d45466cdf418f2abdf2c07b6c76235321e10
Chris <chrisbarbz17@gmail.com>
2022-11-09 14:50:54 +0000

    relationship Created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
