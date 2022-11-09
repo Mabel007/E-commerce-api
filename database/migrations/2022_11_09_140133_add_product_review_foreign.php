7b281d378d590a754b7188e7f0b44cc72e41647b
Chris <chrisbarbz17@gmail.com>
2022-11-09 14:04:37 +0000

    Migration created for product and reviews

---
+++ database/migrations/2022_11_09_140133_add_product_review_foreign.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProductReviewForeign extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+            $table->dropColumn('product_id');
+        });
+    }
+}
