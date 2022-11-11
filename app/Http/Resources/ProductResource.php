82a6ee68104c0fd2c766afd8e236dee47b866481
Chris <chrisbarbz17@gmail.com>
2022-11-11 12:04:57 +0000

    Modified Product Details

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out Of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->discount,2),
+            'rating' => $this->reviews->count() > 0 ?round($this->reviews->sum('star')/$this->reviews->count(),1) : 'No Rating Yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
+
         ];
     }
 }
